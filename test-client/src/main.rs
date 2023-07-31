0d1a8b4b3dd668bf8d2c4336da2e5ef27580ee42
Bayley Foster <bayley@fossec.com>
2023-07-31 15:35:06 +0930

    Add test-client for example radar data

---
+++ test-client/src/main.rs
@@ -0,0 +1,39 @@
+use std::{thread, time};
+use json::object;
+use tungstenite::{connect, Message};
+use url::{Url};
+use rand::Rng;
+
+fn main()  {
+    let (mut socket, _response) = connect(Url::parse("ws://localhost:27017").unwrap())
+        .expect("[test-client] cannot connect to socket");
+
+    loop {
+        let mut _x = json::Array::new();
+        let mut _y = json::Array::new();
+        let mut _health = json::Array::new();
+        let mut _team = json::Array::new();
+        let mut _dormant = json::Array::new();
+        let mut rng = rand::thread_rng();
+
+        for i in 0..10 {
+            _x.push(json::from(rng.gen_range(0.0..1000.0)));
+            _y.push(json::from(rng.gen_range(0.0..1000.0)));
+            _health.push(json::from(rng.gen_range(0.0..100.0)));
+            _team.push(json::from(rng.gen_range(0..1)));
+            _dormant.push(json::from(0));
+        }
+
+        let data = object! {
+            "pos-x": _x,
+            "pos-y": _y,
+            "health": _health,
+            "team": _team,
+            "dormant": _dormant,
+        };
+
+        socket.write_message(Message::from(data.dump())).unwrap();
+
+        thread::sleep(time::Duration::from_millis(1000));
+    }
+}
