d99f3f446360e1af6d19f6da45cacc24b5d08283
Bayley Foster <bayley@fossec.com>
2023-09-13 20:42:50 +0930

    #4 add: rotation and scoped to test-client

--- test-client/src/main.rs
+++ test-client/src/main.rs
@@ -14,6 +14,8 @@ fn main() {
         let mut _health = json::Array::new();
         let mut _team = json::Array::new();
         let mut _dormant = json::Array::new();
+        let mut _rotation = json::Array::new();
+        let mut _scoped = json::Array::new();
         let mut rng = rand::thread_rng();
 
         for _i in 0..10 {
@@ -22,6 +24,8 @@ fn main() {
             _health.push(json::from(rng.gen_range(0.0..100.0)));
             _team.push(json::from(rng.gen_range(0..2)));
             _dormant.push(json::from(0));
+            _rotation.push(json::from(rng.gen_range(0.0..360.0)));
+            _scoped.push(json::from(rng.gen_range(0..2)));
         }
 
         let data = object! {
@@ -30,6 +34,8 @@ fn main() {
             "health": _health,
             "team": _team,
             "dormant": _dormant,
+            "rotation": _rotation,
+            "scoped": _scoped,
         };
 
         socket.write_message(Message::from(data.dump())).unwrap();
