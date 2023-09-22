412b4731cc1d720f8501d5c0a127844be3448402
Henry Nguyen <henmmi@outlook.com>
2023-09-22 15:56:28 +0930

    #9 feat(test-client): add t_score, ct_score, kda, acs, shield, credits

--- test-client/src/main.rs
+++ test-client/src/main.rs
@@ -17,6 +17,13 @@ fn main() {
         let mut _rotation = json::Array::new();
         let mut _scoped = json::Array::new();
         let mut _weapon = json::Array::new();
+        let mut _kill = json::Array::new();
+        let mut _death = json::Array::new();
+        let mut _assist = json::Array::new();
+        let mut _acs = json::Array::new();
+        let mut _shield = json::Array::new();
+        let mut _credits = json::Array::new();
+
         let mut rng = rand::thread_rng();
 
         for _i in 0..rand_player_number {
@@ -28,9 +35,15 @@ fn main() {
             _rotation.push(json::from(rng.gen_range(0.0..360.0)));
             _scoped.push(json::from(rng.gen_range(0..2)));
             _weapon.push(json::from(rng.gen_range(0..100)));
+            _kill.push(json::from(rng.gen_range(0..30)));
+            _death.push(json::from(rng.gen_range(0..30)));
+            _assist.push(json::from(rng.gen_range(0..30)));
+            _acs.push(json::from(rng.gen_range(0..400)));
+            _shield.push(json::from(rng.gen_range(0..50)));
+            _credits.push(json::from(rng.gen_range(0..16000)));
         }
 
-        let data = object! {
+        let players = object! {
             "x": _x,
             "y": _y,
             "health": _health,
@@ -39,6 +52,22 @@ fn main() {
             "rotation": _rotation,
             "scoped": _scoped,
             "weapon": _weapon,
+        };
+
+        let mut _t_score = json::Array::new();
+        let mut _ct_score = json::Array::new();
+
+        _t_score.push(json::from(rng.gen_range(0..16)));
+        _ct_score.push(json::from(rng.gen_range(0..16)));
+
+        let game_info = object! {
+            "t_score": _t_score,
+            "ct_score": _ct_score,
+        };
+
+        let data = object! {
+            "players": players,
+            "game_info": game_info,
         };
 
         socket.write_message(Message::from(data.dump())).unwrap();
