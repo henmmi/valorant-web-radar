2c0653fb5e0d32d8c3bf6746106f81cb78fc04b1
Henry Nguyen <henmmi@outlook.com>
2023-09-26 13:46:54 +0930

    #13 feat: add dormant and killed icons

--- canvas/index.js
+++ canvas/index.js
@@ -2,5 +2,6 @@ import('./pkg')
   .catch(console.error);
 require.context('./src/assets/maps', false, /\.(png)$/);
 require.context('./src/assets/agents', false, /\.(png)$/);
+require.context('./src/assets/icons', false, /\.(png)$/);
 
 import "./src/css/style.css";
