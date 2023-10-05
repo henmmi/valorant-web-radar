22351e587fe81476646e0ca68c1c4cf2a7c4053a
Henry Nguyen <henmmi@outlook.com>
2023-10-05 18:34:45 +1030

    #19 feat: add more maps

--- canvas/src/components/game_data.rs
+++ canvas/src/components/game_data.rs
@@ -17,6 +17,9 @@ pub enum Map {
     Haven,
     Icebox,
     Split,
+    Lotus,
+    Pearl,
+    Fracture,
 }
 impl Map {
     pub fn get_string(&self) -> String {
@@ -27,6 +30,9 @@ impl Map {
             Map::Haven => "Haven".to_string(),
             Map::Icebox => "Icebox".to_string(),
             Map::Split => "Split".to_string(),
+            Map::Lotus => "Lotus".to_string(),
+            Map::Pearl => "Pearl".to_string(),
+            Map::Fracture => "Fracture".to_string(),
         }
     }
 }
